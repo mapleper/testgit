cdaa6f8ed20a2a2c81c04ac5e6646277c7076911
maple <xx@163.com>
2020-04-05 14:59:17 +0800

    123

--- src/main/java/demo01/MyRunnable.java
+++ src/main/java/demo01/MyRunnable.java
@@ -7,7 +7,7 @@ public class MyRunnable implements Runnable {
     public void run() {
 		int a =11;
 		int b =13;
-		int c =15;
+		int c =14;
         synchronized (this){
             for (int i = 0; i < 5; i++) {
                 System.out.println("当前线程:" + Thread.currentThread().getName() + "---num:" + num);
